d02f72c91f577bbdf286df14b909f54b90e8c869
wbydc <root@wbydc.io>
2022-05-04 16:20:35 +0200

    Fix getProvider

--- src/models/Offer.ts
+++ src/models/Offer.ts
@@ -42,7 +42,8 @@ class Offer {
      * Function for fetching offer provider from blockchain (works for TEE and Value offers)
      */
     public async getProvider(): Promise<string> {
-        return await this.contract.methods.getProviderAuthority();
+        this.providerAuthority = await this.contract.methods.getOfferProviderAuthority(this.offerId).call();
+        return this.providerAuthority!;
     }
 
     /**

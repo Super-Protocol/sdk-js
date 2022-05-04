b6f79fdc1cd9189fcbd7419787e52738dcaf9155
wbydc <root@wbydc.io>
2022-05-04 11:21:37 +0200

    String to number

--- src/TIIGenerator.ts
+++ src/TIIGenerator.ts
@@ -85,7 +85,7 @@ class TIIGenerator {
     }
 
     public static async generate(
-        orderId: string,
+        orderId: number,
         resource: Resource,
         args: any,
         encryption: Encryption
@@ -93,7 +93,7 @@ class TIIGenerator {
         const order: Order = new Order(orderId);
 
         const parentOrderAddress: string = await order.getParentOrder();
-        const parentOrder: Order = new Order(parentOrderAddress);
+        const parentOrder: Order = new Order(+parentOrderAddress);
         const parentOrderInfo: OrderInfo = await parentOrder.getOrderInfo();
 
         const solutionHashes: Hash[] = [];

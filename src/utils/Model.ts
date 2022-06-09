eacea5fa329339bb69f814d83dabe3faef58660f
Ilya Shmelev <ishmelev23@gmail.com>
2022-06-09 16:33:43 +0300

    Added logging for transactions publishing

--- src/utils/Model.ts
+++ src/utils/Model.ts
@@ -1,10 +1,12 @@
 import { TransactionReceipt } from "web3-core";
 import { ContractSendMethod } from "web3-eth-contract";
 import NonceTracker from "./NonceTracker";
+import rootLogger from "../logger";
 import store from "../store";
 import { TransactionOptions } from "../types/Web3";
 import { checkIfActionAccountInitialized, checkIfInitialized, createTransactionOptions } from "../utils";
 import Superpro from "../staticModels/Superpro";
+import lodash from "lodash";
 
 type ArgumentsType = any | any[];
 
@@ -24,6 +26,7 @@ abstract class Model {
         checkIfInitialized();
         checkIfActionAccountInitialized(transactionOptions);
 
+        const logger = rootLogger.child({ className: "Model" });
         const web3 = transactionOptions?.web3 || store.web3;
 
         if (!web3) {
@@ -40,7 +43,7 @@ abstract class Model {
         const nonce = await NonceTracker.generateNextNonce(rawOptions.from);
 
         try {
-            const options = {
+            const txData = {
                 to,
                 data: transaction.encodeABI(),
                 nonce: nonce.nextNonce,
@@ -49,14 +52,29 @@ abstract class Model {
 
             if (store.keys[rawOptions.from]) {
                 const key = store.keys[rawOptions.from];
-                const signed = await web3.eth.accounts.signTransaction(options, key);
+                const signed = await web3.eth.accounts.signTransaction(txData, key);
                 if (!signed.rawTransaction) {
                     throw new Error("Failed to sign transaction");
                 }
 
+                logger.debug(
+                    {
+                        txHash: signed.transactionHash,
+                        txData: lodash.omit(txData, ["data"]),
+                    },
+                    "Publishing signed transaction",
+                );
+
                 return await web3.eth.sendSignedTransaction(signed.rawTransaction);
             } else {
-                return await web3.eth.sendTransaction(options);
+                logger.debug(
+                    {
+                        txData: lodash.omit(txData, ["data"]),
+                    },
+                    "Publishing unsigned transaction",
+                );
+
+                return await web3.eth.sendTransaction(txData);
             }
         } finally {
             nonce.done();

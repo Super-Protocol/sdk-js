562731817a4f7236a669f5aa0d2c04e99d64f953
YakovlevAlexander <yakovlev.alexander.work@gmail.com>
2023-01-12 12:10:32 +0300

    Delete Order.test.ts

--- test/Order.test.ts
+++
@@ -1,9 +0,0 @@
-import Order from "../src/models/Order";
-
-describe("Order", () => {
-    test("throws error for non-existing order", async () => {
-        const order = new Order("testNonExistingOrder");
-
-        await expect(order.getOrderInfo()).rejects.toThrowError("Order testNonExistingOrder does not exist");
-    });
-});

6f99c249c064f917002a7faa283c7d06f177113d
Marat Idrisov <43449302+Idris0v@users.noreply.github.com>
2023-01-20 16:14:20 +0300

    Option to disable logger (#126)

--- src/logger.ts
+++ src/logger.ts
@@ -1,5 +1,8 @@
 import pino from "pino";
 
-const logger = pino({ level: process.env.LOG_LEVEL || "trace" });
+const logger = pino({
+    level: process.env.LOG_LEVEL || "trace",
+    enabled: process.env.DISABLE_LOGGER !== "true",
+});
 
 export default logger;

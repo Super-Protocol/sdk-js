c059c00d292b1b8b842525646ee31d1b1ac0eda8
Petr Evstifeev <7293601+Villain88@users.noreply.github.com>
2023-07-16 23:02:41 +0300

    feature/SP-3174, implemented thread limit

--- src/providers/storage/getStorageProvider.ts
+++ src/providers/storage/getStorageProvider.ts
@@ -7,7 +7,7 @@ export default (storageAccess: StorageAccess): IStorageProvider => {
     const key = storageAccess.storageType as StorageType;
     switch (key) {
         case StorageType.StorJ:
-            return new StorjStorageProvider(storageAccess.credentials);
+            return new StorjStorageProvider(storageAccess.credentials, storageAccess.maximumConcurrent);
         default:
             throw Error(`Unsupported storageType ${key}`);
     }
--- src/types/storage/StorageAccess.ts
+++ src/types/storage/StorageAccess.ts
@@ -3,6 +3,7 @@ import { StorageType } from "@super-protocol/dto-js";
 type StorageAccess = {
     storageType: StorageType;
     credentials: any;
+    maximumConcurrent?: number
 };
 
 export default StorageAccess;

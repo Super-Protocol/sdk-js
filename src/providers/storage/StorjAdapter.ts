e442f8fc46aed53f9ef369c17b69b2dc51ff4026
galkinsden <galkinsden@gmail.com>
2023-08-15 09:45:52 +0300

    add adapters tests

--- src/providers/storage/StorjAdapter.ts
+++ src/providers/storage/StorjAdapter.ts
@@ -16,7 +16,7 @@ export default class StorjAdapter<V extends object> {
     public subscribe(cb: (props: { type: CacheEvents; message: unknown }) => void) {
         return this.storageAdapter?.subscribe(cb);
     }
-    public async get(key: string, encryptionKey: Buffer) {
+    public async get(key: string, encryptionKey: Buffer): Promise<(V | null)[] | null> {
         return this.storageAdapter.get(key, encryptionKey).catch((err: Error) => {
             const message = err.message?.toLowerCase() || "";
             if (message.includes("object not found") || message.includes("object has been deleted")) {
@@ -27,27 +27,27 @@ export default class StorjAdapter<V extends object> {
             throw err;
         });
     }
-    public async has(key: string) {
+    public async has(key: string): Promise<boolean> {
         return this.storageAdapter.has(key);
     }
-    public async set(key: string, value: V, encryptionKey: Buffer) {
+    public async set(key: string, value: V, encryptionKey: Buffer): Promise<void> {
         return this.storageAdapter.set(key, value, encryptionKey).catch((err: Error) => {
             const message = err.message?.toLowerCase() || "";
             if (message.includes("object has been deleted")) {
                 this.logger?.info({ key }, "Object has been deleted");
 
-                return null;
+                return;
             }
             throw err;
         });
     }
-    public async del(key: string) {
+    public async del(key: string): Promise<void> {
         return this.storageAdapter.delete(key);
     }
-    public stop() {
+    public stop(): void {
         this.storageAdapter.stop();
     }
-    public async shutdown() {
+    public async shutdown(): Promise<void> {
         await this.storageAdapter.shutdown();
     }
 }

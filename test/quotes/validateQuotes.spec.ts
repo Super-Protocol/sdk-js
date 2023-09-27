62c373bd1dbe32485a45cac3ce44b5b5034e6942
YakovlevAlexander <yakovlev.alexander.work@mail.com>
2023-09-27 23:09:09 +0400

    comment tests

--- test/quotes/validateQuotes.spec.ts
+++ test/quotes/validateQuotes.spec.ts
@@ -1,29 +1,29 @@
-import { QuoteValidator } from '../../src/tee/QuoteValidator';
-import { QuoteValidationStatuses } from '../../src/tee/statuses';
-import { testQuotes } from './examples';
+// import { QuoteValidator } from '../../src/tee/QuoteValidator';
+// import { QuoteValidationStatuses } from '../../src/tee/statuses';
+// import { testQuotes } from './examples';
 
-// TODO: mock network queries and cover all possible states
-xdescribe('Quote validatator', () => {
-    const validator = new QuoteValidator();
+// // TODO: mock network queries and cover all possible states
+// describe('Quote validatator', () => {
+//     const validator = new QuoteValidator();
 
-    test('test quote', async () => {
-        const quoteBuffer = Buffer.from(testQuotes.testQuote, 'base64');
-        const res = await validator.validate(quoteBuffer);
-        expect(res).toBeDefined();
-        expect(res.quoteValidationStatus).toEqual(QuoteValidationStatuses.SecurityPatchNeeded);
-    }, 10000);
+//     test('test quote', async () => {
+//         const quoteBuffer = Buffer.from(testQuotes.testQuote, 'base64');
+//         const res = await validator.validate(quoteBuffer);
+//         expect(res).toBeDefined();
+//         expect(res.quoteValidationStatus).toEqual(QuoteValidationStatuses.SecurityPatchNeeded);
+//     }, 10000);
 
-    test('provisioner quote', async () => {
-        const quoteBuffer = Buffer.from(testQuotes.provisionerQuote, 'base64');
-        const res = await validator.validate(quoteBuffer);
-        expect(res).toBeDefined();
-        expect(res.quoteValidationStatus).toEqual(QuoteValidationStatuses.SecurityPatchNeeded);
-    }, 10000);
+//     test('provisioner quote', async () => {
+//         const quoteBuffer = Buffer.from(testQuotes.provisionerQuote, 'base64');
+//         const res = await validator.validate(quoteBuffer);
+//         expect(res).toBeDefined();
+//         expect(res.quoteValidationStatus).toEqual(QuoteValidationStatuses.SecurityPatchNeeded);
+//     }, 10000);
 
-    test('tunnel quote', async () => {
-        const quoteBuffer = Buffer.from(testQuotes.tunnelQuote, 'base64');
-        const res = await validator.validate(quoteBuffer);
-        expect(res).toBeDefined();
-        expect(res.quoteValidationStatus).toEqual(QuoteValidationStatuses.SecurityPatchNeeded);
-    }, 10000);
-});
+//     test('tunnel quote', async () => {
+//         const quoteBuffer = Buffer.from(testQuotes.tunnelQuote, 'base64');
+//         const res = await validator.validate(quoteBuffer);
+//         expect(res).toBeDefined();
+//         expect(res.quoteValidationStatus).toEqual(QuoteValidationStatuses.SecurityPatchNeeded);
+//     }, 10000);
+// });

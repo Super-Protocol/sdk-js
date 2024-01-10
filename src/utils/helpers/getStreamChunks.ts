2713b52d8e2e85698af12393b2874f75d584a647
Marat Idrisov <idrisovm15@gmail.com>
2024-01-10 08:20:22 -0500

    renames

--- src/utils/helpers/getStreamChunks.ts
+++ src/utils/helpers/getStreamChunks.ts
@@ -8,7 +8,7 @@ export interface ChunkPart {
 
 export async function* getStreamChunks(
   data: Readable,
-  partSize: number,
+  chunkSize: number,
 ): AsyncGenerator<ChunkPart, void, undefined> {
   let partNumber = 1;
   const currentBuffer: { chunks: Buffer[]; length: number } = { chunks: [], length: 0 };
@@ -17,7 +17,7 @@ export async function* getStreamChunks(
     currentBuffer.chunks.push(buffer);
     currentBuffer.length += buffer.length;
 
-    while (currentBuffer.length >= partSize) {
+    while (currentBuffer.length >= chunkSize) {
       /**
        * Concat all the buffers together once if there is more than one to concat. Attempt
        * to minimize concats as Buffer.Concat is an extremely expensive operation.
@@ -29,11 +29,11 @@ export async function* getStreamChunks(
 
       yield {
         partNumber,
-        data: dataChunk.slice(0, partSize),
+        data: dataChunk.slice(0, chunkSize),
       };
 
       // Reset the buffer.
-      currentBuffer.chunks = [dataChunk.slice(partSize)];
+      currentBuffer.chunks = [dataChunk.slice(chunkSize)];
       currentBuffer.length = currentBuffer.chunks[0].length;
       partNumber += 1;
     }

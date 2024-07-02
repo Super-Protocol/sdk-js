d1f36e3e31ed7f6d59401b64e039122b5ed98f2b
serega-k <zloebuch@gmail.com>
2024-07-02 13:10:48 +0300

    Revert "fix(OffersStorageAllocated): getByIssuerId should not return empty requests  (SP-4071)"
    This reverts commit 89323022ac288a9d561aaf8738282b78dbceb6f1.

--- src/staticModels/OffersStorageAllocated.ts
+++ src/staticModels/OffersStorageAllocated.ts
@@ -28,11 +28,9 @@ class OffersStorageAllocated {
       .getStorageOrdersAllocatedByIssuer(teeOfferIssuerId)
       .call()
       .then((allocatedOffers: unknown[] | void) =>
-        allocatedOffers!
-          .map((allocated) =>
-            convertOfferStorageAllocatedFromRaw(allocated as OfferStorageAllocated),
-          )
-          .filter((allocated) => Number(allocated.timestamp) !== 0),
+        allocatedOffers!.map((allocated) =>
+          convertOfferStorageAllocatedFromRaw(allocated as OfferStorageAllocated),
+        ),
       );
 
     return allocated;

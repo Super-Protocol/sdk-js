b8c51a154486b283c338aa34835f3e21d4387c1c
serega-k <zloebuch@gmail.com>
2024-09-03 21:26:34 +0300

    refactor(errors): import lodash

--- src/errors/utils.ts
+++ src/errors/utils.ts
@@ -1,18 +1,18 @@
-import { isObject, get, toString } from 'lodash';
+import _ from 'lodash';
 
 export const getErrorMessage = (error: unknown): string => {
   if (typeof error === 'string') {
     return error;
   }
   const allowedErrorProperties = ['message', 'stack'];
-  if (isObject(error)) {
+  if (_.isObject(error)) {
     for (const field of allowedErrorProperties) {
-      const value = get(error, field);
+      const value = _.get(error, field);
       if (typeof value === 'string') {
         return value;
       }
     }
-    return toString(error);
+    return _.toString(error);
   }
   return `Unknown error`;
 };
